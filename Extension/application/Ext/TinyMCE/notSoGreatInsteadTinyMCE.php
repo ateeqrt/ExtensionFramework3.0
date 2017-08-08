a6dbcd03d91f2af494076707a7083ad1abc67c6e
ateeqrt <ateeq.rehman@rolustech.net>
2017-08-08 14:20:38 +0500

    TinyMCE

---
+++ Extension/application/Ext/TinyMCE/notSoGreatInsteadTinyMCE.php
@@ -0,0 +1,8 @@
+<?php
+$defaultConfig['height'] = '1000';
+$buttonConfig['default'] = array(
+	'buttonConfig' => "bold,italic,underline,strikethrough,separator,bullist,numlist",
+	'buttonConfig2' => "justifyleft,justifyright,justifycenter,justifyfull",
+	'buttonConfig3' => "fontselect,fontsizeselect",
+	);
+$pluginsConfig['default'] = "advhr,preview,paste,directionality";
---
+++ application/Ext/TinyMCE/tinymce.ext.php
@@ -0,0 +1,14 @@
+<?php
+// WARNING: The contents of this file are auto-generated.
+?>
+<?php
+// Merged from custom/Extension/application/Ext/TinyMCE/notSoGreatInsteadTinyMCE.php
+
+$defaultConfig['height'] = '1000';
+$buttonConfig['default'] = array(
+	'buttonConfig' => "bold,italic,underline,strikethrough,separator,bullist,numlist",
+	'buttonConfig2' => "justifyleft,justifyright,justifycenter,justifyfull",
+	'buttonConfig3' => "fontselect,fontsizeselect",
+	);
+$pluginsConfig['default'] = "advhr,preview,paste,directionality";
+?>

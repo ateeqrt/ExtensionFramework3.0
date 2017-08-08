a326f92acd920909b77cc53e8afa784e729592fa
ateeqrt <ateeq.rehman@rolustech.net>
2017-08-08 12:57:13 +0500

    SideCar Extension

---
+++ Extension/modules/clg_Programs/Ext/clients/base/layouts/subpanels/notSoAwesomeSidecarExtension.php
@@ -0,0 +1,8 @@
+<?php
+$viewdefs['clg_Programs']['base']['layout']['subpanels']['components'][] = array(
+	'layout' => 'subpanel',
+	'label' => 'LBL_CLG_PROGRAMS_CLG_DISCIPLINES_FROM_CLG_DISCIPLINES_TITLE',
+	'context' => array(
+		'link' => 'clg_Programs_clg_disciplines',
+		),
+	);
--- modules/clg_Programs/Ext/clients/base/layouts/subpanels/subpanels.ext.php
+++ modules/clg_Programs/Ext/clients/base/layouts/subpanels/subpanels.ext.php
@@ -11,3 +11,11 @@ $viewdefs['clg_Programs']['base']['layout']['subpanels']['components'][] = array
     'link' => 'clg_programs_clg_disciplines',
   ),
 );
+
+$viewdefs['clg_Programs']['base']['layout']['subpanels']['components'][] = array(
+	'layout' => 'subpanel',
+	'label' => 'LBL_CLG_PROGRAMS_CLG_DISCIPLINES_FROM_CLG_DISCIPLINES_TITLE',
+	'context' => array(
+		'link' => 'clg_Programs_clg_disciplines',
+		),
+	);
